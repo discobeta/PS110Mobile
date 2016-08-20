0d62c303d077d2291c05267d8bb43d1bff7f5259
Asaf Klibansky <discobeta@gmail.com>
2016-08-20 11:14:24 -0400

    set backend for ps110.org

--- app/providers/user-data/user-data.ts
+++ app/providers/user-data/user-data.ts
@@ -4,12 +4,7 @@ import {Http, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
-/*
-  Generated class for the UserData provider.
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class UserData {
   data: any = null;
